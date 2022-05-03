4e9a6c5ff0d10b9ef5e0b050a81b034a9aec0fd8
mktcode <kontakt@markus-kottlaender.de>
2022-05-03 10:09:44 +0200

    updated deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Boost = await ethers.getContractFactory("Boost");
+  const boost = await Boost.deploy();
 
-  await greeter.deployed();
+  await boost.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Boost deployed to:", boost.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
