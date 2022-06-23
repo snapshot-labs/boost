84d9bf49d36486519f7564dc84c5834be8f8d935
mktcode <kontakt@markus-kottlaender.de>
2022-06-23 17:34:48 +0200

    removed unused import

--- contracts/Boost.sol
+++ contracts/Boost.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "./IBoost.sol";
 
