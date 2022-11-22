ab4f7426a463af341794936b922781c0e55f75de
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-11-22 16:20:33 +0000

    updated foundry config

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -8,12 +8,12 @@ interface SingletonFactory {
     function deploy(bytes memory _initCode, bytes32 salt) external returns (address payable);
 }
 
-contract MyScript is Script {
+contract BoostSingletonDeployer is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         SingletonFactory singletonFactory = SingletonFactory(0xce0042B868300000d44A59004Da54A005ffdcf9f);
-        bytes32 salt = bytes32(uint256(1));
+        bytes32 salt = bytes32(uint256(2));
         singletonFactory.deploy(type(Boost).creationCode, salt);
 
         vm.stopBroadcast();
