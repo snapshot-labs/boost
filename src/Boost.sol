e1ee525dc3cfc382c7fa30fa1f0deb873d87a127
Orland0x <37511817+Orland0x@users.noreply.github.com>
2023-01-09 15:41:58 -0500

    chore: use final OZ EIP712 impl (#23)
    * chore: use final OZ EIP712 impl
    
    * chore: Remove draft EIP712 from tests

--- src/Boost.sol
+++ src/Boost.sol
@@ -13,7 +13,7 @@ pragma solidity ^0.8.15;
 
 import "openzeppelin-contracts/access/Ownable.sol";
 import "openzeppelin-contracts/utils/cryptography/SignatureChecker.sol";
-import "openzeppelin-contracts/utils/cryptography/draft-EIP712.sol";
+import "openzeppelin-contracts/utils/cryptography/EIP712.sol";
 import "openzeppelin-contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
 
